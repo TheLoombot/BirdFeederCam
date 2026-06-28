102972e48e85cffcec7a987a59720d54a45b850f
TheLoombot <4617505+TheLoombot@users.noreply.github.com>
2026-06-27 21:42:49 -0400

    Initial Commit

---
+++ BirdFeederCam/BirdFeederCamApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirdFeederCamApp.swift
+//  BirdFeederCam
+//
+//  Created by Aman on 6/27/26.
+//
+
+import SwiftUI
+
+@main
+struct BirdFeederCamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BirdFeederCam/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  BirdFeederCam
+//
+//  Created by Aman on 6/27/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
